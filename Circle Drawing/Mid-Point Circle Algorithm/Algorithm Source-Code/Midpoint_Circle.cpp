b3aa84f12d81503bf1182fb1a3fdc6c07a62b790
Vedant Bonde <vedantbonde19@gmail.com>
2020-03-13 11:42:08 +0530

    Circle Drawing Algorithms

---
+++ Circle Drawing/Mid-Point Circle Algorithm/Algorithm Source-Code/Midpoint_Circle.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <GL/glut.h>
+
+using namespace std;
+
+int pntX1, pntY1, pntX2, pntY2, r;
+
+int roundoff(double d)
+{
+  return floor(d + 0.5);
+}
+
+void plot(int x, int y)
+{
+	glBegin(GL_POINTS);
+	glVertex2i(x + pntX1, y + pntY1);
+	glEnd();
+}
+
+
+void myInit (void)
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	//glColor3f(0.0f, 0.0f, 0.0f);
+	glColor3f (1.0, 1.0, 1.0);
+	glPointSize(4.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, 640.0, 0.0, 480.0);
+}
+
+void Mid_Circle (int xBeg, int yBeg, int r)
+{
+	int xp = 0;
+	int yp = r;
+	int dec_par = 5/4 - r;
+	plot(xp, yp);
+
+	while(yp > xp){
+		if(dec_par < 0){
+			xp = xp + 1;
+			dec_par = dec_par + 2*(xp) + 1;
+		}
+		else{
+			yp = yp - 1;
+			xp = xp + 1;
+			dec_par = dec_par + 2*(xp - yp) + 1;
+		}
+
+
+		plot(xp, yp);
+		plot(xp, -yp);
+		plot(-xp, yp);
+		plot(-xp, -yp);
+		plot(yp, xp);
+		plot(yp, -xp);
+		plot(-yp, xp);
+		plot(-yp, -xp);
+		
+	}
+
+}
+
+void drawLine()
+{
+	glBegin(GL_LINES);
+	glVertex2i(pntX1, pntY1+30);
+	glVertex2i(pntX2, pntY2+30);
+	glEnd();
+}
+
+void myDisplay(void)
+{
+	glClear (GL_COLOR_BUFFER_BIT);
+	glColor3f (1.0, 1.0, 1.0);
+	glPointSize(1.0);
+	Mid_Circle(pntX1, pntY1, r);
+	//drawLine();
+	glFlush ();
+}
+
+int main(int argc, char** argv)
+{
+
+	cout << "Enter the coordinates of the center: ";
+
+	cin >> pntX1;
+	cin >> pntY1;
+	cout << "\nEnter radius : "; 
+	cin >> r;
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize (640, 480);
+	glutInitWindowPosition (100, 150);
+	glutCreateWindow ("Midpoint Circle Drawing Alogrithm");
+	glutDisplayFunc(myDisplay);
+	myInit ();
+	glutMainLoop();
+
+}
